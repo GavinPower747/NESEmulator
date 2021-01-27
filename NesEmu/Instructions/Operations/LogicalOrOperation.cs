8637df8885972f1dfaf09bd0e479ca1348756ad3
GavinPower747 <gavinpower747@hotmail.com>
2021-01-27 00:00:34 +0000

    Renamed Operation

--- NesEmu/Instructions/Operations/LogicalOrOperation.cs
+++ NesEmu/Instructions/Operations/LogicalOrOperation.cs
@@ -4,11 +4,11 @@ using NesEmu.Extensions;
 
 namespace NesEmu.Instructions.Operations
 {
-    public class OrAccumulatorOperation : IOperationStrategy
+    public class LogicalOrOperation : IOperationStrategy
     {
         public int Operate(byte data, CPURegisters registers, IBus bus)
         {
-            registers.Accumulator = Convert.ToByte(data | registers.Accumulator);
+            registers.Accumulator = (byte)(data | registers.Accumulator);
 
             registers.StatusRegister.SetFlag(StatusRegister.Zero, registers.Accumulator == 0x00);
             registers.StatusRegister.SetFlag(StatusRegister.Negative, Convert.ToBoolean(registers.Accumulator & 0x80));
