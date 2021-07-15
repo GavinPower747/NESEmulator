9eef7dc88e8116daa0cf351f39a21ad11dbc0efe
GavinPower747 <gavinpower747@hotmail.com>
2021-07-16 00:08:07 +0100

    Added initial cart impl to test CPU

---
+++ NesEmu.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NesEmu.Core;
+
+namespace NesEmu.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var console = new NintendoEntertainmentSystem();
+
+            console.LoadCartridge("D:\\Dev\\NesEmulator\\TestRoms\\official.nes");
+        }
+    }
+}
--- NesEmu/Devices/Cartridge/Cartridge.cs
+++ NesEmu/Devices/Cartridge/Cartridge.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Collections.Generic;
 
@@ -5,6 +6,8 @@ namespace NesEmu.Devices.Cartridge
 {
     public class Cartridge
     {
+        private List<byte> _programBody;
+        private byte[] _characterRom;
         private string _pathToRom;
 
         public Cartridge(string path)
@@ -17,7 +20,7 @@ namespace NesEmu.Devices.Cartridge
             using (var fileStream = new FileStream(_pathToRom, FileMode.Open))
             using (var reader = new BinaryReader(fileStream))
             {
-                List<byte> romBody = new List<byte>();
+                _programBody = new List<byte>();
 
                 //Read the 5 byte cart header
                 byte programBankCount = reader.ReadByte();
@@ -28,8 +31,41 @@ namespace NesEmu.Devices.Cartridge
 
                 reader.ReadBytes(7); //7 empty bytes after the header, move the reader along...
 
-                
+                int mirrorLowBit = control1 & 1;
+                int mirrorHighBit = (control1 >> 3) & 1;
+
+                byte mirrorModeByte = (byte)((mirrorHighBit << 1) | mirrorLowBit);
+
+                var mapperIdLo = control1 >> 4;
+                int mapperIdHi = control2 >> 4;
+                var mapperId = (byte)((mapperIdHi << 4) | mapperIdLo);
+
+                var programData = reader.ReadBytes(programBankCount * 0x4000);
+                _programBody.AddRange(programData);
+
+                if(characterBankCount == 0)
+                {
+                    _characterRom = new byte[0x2000];
+                }                
+                else
+                {
+                    _characterRom = reader.ReadBytes(characterBankCount * 0x2000);
+                }
+
+                OutputProgramData(programBankCount, characterBankCount, control1, control2, programSize, mapperId, mirrorModeByte);
             }
+        }
+
+        private void OutputProgramData(byte programBankCount, byte characterBankCount, byte control1, byte control2, byte programSize, byte mapperId, byte mirrorModeByte)
+        {
+            Console.WriteLine($"ProgramBankCount: {programBankCount.ToString()}");
+            Console.WriteLine($"CharacterBankCount: {characterBankCount.ToString()}");
+            Console.WriteLine($"control1: {Convert.ToString(control1, 2).PadLeft(8, '0')}");
+            Console.WriteLine($"control2: {Convert.ToString(control2, 2).PadLeft(8, '0')}");
+            Console.WriteLine($"ProgramSize: {programSize.ToString()}");
+            Console.WriteLine($"MapperId: {mapperId.ToString()}");
+            Console.WriteLine($"Mirror Mode: {mirrorModeByte}");
+            Console.WriteLine($"Program Data: {BitConverter.ToString(_programBody.ToArray())}");
         }
     }
 }
