cc6d8b57dd8ac06874cb782ef57a0d1df8d8e678
GavinPower747 <gavinpower747@hotmail.com>
2021-09-24 01:25:24 +0100

    Added bit ranges to byte register

---
+++ NesEmu.Tests/Core/ByteRegisterTests.cs
@@ -0,0 +1,89 @@
+using System;
+using NesEmu.Core;
+using NUnit.Framework;
+
+namespace NesEmu.Tests.Core
+{
+    [TestFixture]
+    public class ByteRegisterTests
+    {
+        [Test]
+        public void Can_Set_Flag()
+        {
+            TestRegister testRegister = new();
+
+            testRegister.FourthBit = true;
+
+            Assert.That(testRegister.FourthBit, Is.True);
+        }
+
+        [Test]
+        public void Can_Get_Flag()
+        {
+            byte testByte = 0b000010000;
+            TestRegister testRegister = new(testByte);
+            
+            var bit = testRegister.FourthBit;
+
+            Assert.That(bit, Is.EqualTo((testByte & (1 << 4)) == 0));
+        }
+
+        [Test]
+        public void Can_Set_BitRange()
+        {
+            TestRegister testRegister = new(0b11111111);
+
+            testRegister.MultiByte = 0b101;
+
+            Assert.That(testRegister.MultiByte, Is.EqualTo(0b101));
+        }
+
+        [Test]
+        public void Set_DoesntChange_OtherBits()
+        {
+            TestRegister register = new(0b11111111);
+
+            register.MultiByte = 0b000;
+            byte backingByte = register;
+
+            Assert.That(backingByte, Is.EqualTo(0b11110001));
+        }
+
+        [Test]
+        public void Set_DoesntChange_OtherBits_When_BitIsOversized()
+        {
+            TestRegister register = new(0b11111111);
+
+            register.MultiByte = 0b00001011;
+            byte backingByte = register;
+
+            Assert.That(backingByte, Is.EqualTo(0b11110111));
+        }
+
+        [Test]
+        public void Can_Get_BitRange()
+        {
+            byte testByte = 0b000001010; 
+            TestRegister testRegister = new(testByte);
+
+            Assert.That(testRegister.MultiByte, Is.EqualTo(0b101));
+        }
+
+
+        public class TestRegister : ByteRegister
+        {
+            public bool Flag { get => GetFlag(0); set => SetFlag(0, value); }
+            public byte MultiByte { get => GetBitRange(1, 3); set=> SetBitRange(1, 3, value); }
+            public bool FourthBit { get => GetFlag(4); set => SetFlag(4, value); } //Yes it's the 5th bit but shush
+
+            public TestRegister(byte initialValues) : base(initialValues)
+            {
+            }
+
+            public TestRegister() : base(0x00)
+            {
+            }
+
+        }        
+    }
+}
--- NesEmu/Core/ByteRegister.cs
+++ NesEmu/Core/ByteRegister.cs
@@ -1,3 +1,4 @@
+using System;
 namespace NesEmu.Core
 {
     public abstract class ByteRegister
@@ -24,6 +25,40 @@ namespace NesEmu.Core
                 _backingByte |= mask;
             else
                 _backingByte &= (byte)~mask;
+        }
+
+        ///<summary>
+        ///Isolates a range of bits and returns them in a new byte
+        ///</summary>
+        ///<remarks>Overly verbose and commented as this took me ages to come up with and wrap my head around</remarks>
+        protected byte GetBitRange(int startIndex, int endIndex)
+        {
+            byte count = (byte)((endIndex - startIndex) + 1);
+            byte shifted = (byte)(_backingByte >> startIndex); //shift the desired bits over to the far right of the byte
+            
+            //if we take the count and subtract 1 we will have a perfect mask to extract our shifted bits
+            //e.g. 1 << 3 = 00001000; 00001000 - 1 = 00000111
+            byte mask = (byte)((1 << count) - 1);
+
+            //extract the bits using the AND operator
+            return (byte)(shifted & mask);
+        }
+
+        ///<summary>
+        ///Takes the first x bits of the supplied byte and sets them over the backing byte
+        ///</summary>
+        ///<remarks>Overly verbose and commented as this took me ages to come up with and wrap my head around</remarks>
+        protected void SetBitRange(int startIndex, int endIndex, byte data)
+        {
+            byte count = (byte)((endIndex - startIndex) + 1);
+            byte mask = (byte)((1 << count) - 1);
+
+            //AND the backing byte with the inverse of the mask, this zeros out the range we're setting while preserving the rest of the byte
+            byte zeroedOut = (byte)(_backingByte & ~(mask << startIndex));
+            byte newBits = (byte)(data & mask); //Only take the right amount of bits
+
+            //Shift the new bits to the correct location and set them on the zeroed version of the original value
+            _backingByte = (byte)(zeroedOut | (newBits << startIndex));
         }
 
         public static implicit operator byte(ByteRegister register) => register._backingByte;
